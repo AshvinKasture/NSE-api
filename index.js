48bd30be7fed82498866dcf4f811a348d9dc32d5
Ashvin Kasture <kashvin525@gmail.com>
2022-01-15 16:34:20 +0530

    almost ready

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
 console.clear();
 
 const express = require('express');
-const finnhub = require('finnhub');
+const { check, validationResult } = require('express-validator');
 const config = require('config');
+const request = require('request');
 
 const app = express();
 
-let finnhubClient;
+let data;
+
+app.use(express.json({ extended: false }));
+
+const getData = () => {
+  try {
+    const options = {
+      uri: `https://finnhub.io/api/v1/stock/symbol?exchange=NS&token=${config.get(
+        'api_key'
+      )}`,
+      method: 'GET',
+    };
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        data = JSON.parse(body);
+        console.log('Data initialised');
+      }
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 app.get('/', (req, res) => {
-  console.log('Sending request');
-  finnhubClient.stockSymbols('US', (error, data, response) => {
-    console.log('got response');
-    console.log(response);
-    if (error) {
-      console.error(error);
-    }
-    console.log(data);
-  });
-  res.send('Hello');
+  res.send('Welcome to NSE api by Ashvin Kasture');
 });
 
-app.listen(5000, () => {
-  const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-  //   api_key.apiKey = config.get('api_key');
-  api_key.apiKey = 'c7goei2ad3ibsjtt0sg0';
-  finnhubClient = new finnhub.DefaultApi();
+app.get(
+  '/query/',
+  [check('term', 'Term is required').not().isEmpty()],
+  (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(401).json({ errors: errors.array() });
+      }
+      let symbols = [];
+      const term = req.body.term;
+      const limit = req.body.limit || 10;
+      data.forEach((item) => {
+        if (
+          item.displaySymbol.toLowerCase().startsWith(term) ||
+          item.description.toLowerCase().startsWith(term)
+        ) {
+          symbols.push(item.displaySymbol);
+        }
+      });
+      return res.json(symbols.slice(0, limit));
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).send('Internal Server Error');
+    }
+  }
+);
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => {
   console.log('Server has started');
+  getData();
 });
